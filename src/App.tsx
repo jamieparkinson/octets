ee6ac4e94a5728c14e49e8581e67da26bc5f7f46
jamie <jamie.parkinson@techwillsaveus.com>
2019-03-31 22:36:56 +0100

    Add byte counter

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,11 @@ const ContentBox = styled.div`
   border: 1px solid rgb(220, 220, 220);
   border-radius: 5px;
   padding: 20px;
-  box-shadow: 0 0 4px rgba(0,0,0,0.1);
+  box-shadow: 0 0 4px rgba(0, 0, 0, 0.1);
   background-color: white;
-  width: 50%;
-  max-width: 400px;
-  min-width: 310px;
+  width: 100%;
+  max-width: 500px;
+  min-width: 350px;
 `;
 
 const Title = styled.h4`
@@ -29,29 +29,47 @@ const Title = styled.h4`
   color: rgb(30, 30, 30);
 `;
 
-type ValueType = number | '';
+const NBytes = styled.span`
+  float: right;
+  color: rgb(200, 200, 200);
+  font-size: 15px;
+  margin-bottom: 6px;
+`;
+
+type ValueType = number | "";
 
 const createValueParser = (setValue: (n: ValueType) => void, radix: number) => {
   return (x: string): void => {
     const v = parseInt(x, radix);
-    setValue(isNaN(v) ? '' : v);
-  }
+    setValue(isNaN(v) ? "" : v);
+  };
 };
 
-const formatValue = (value: ValueType, radix: number, length: number): string => {
+const formatValue = (
+  value: ValueType,
+  radix: number,
+  length: number
+): string => {
   const str = value.toString(radix);
-  if (str !== '') {
-    return str.padStart(length, '0');
+  if (str !== "") {
+    return str.padStart(length, "0");
   }
-  return '';
+  return "";
 };
 
 const App: React.FC = () => {
-  const [ value, setValue ] = useState<ValueType>('');
+  const [value, setValue] = useState<ValueType>("");
   return (
     <AppWrapper>
       <ContentBox>
         <Title>Octet format converter</Title>
+        <NBytes>
+          {value !== "" ? (
+            <>Fits in {Math.ceil(Math.log2(value + 1) / 8)} byte(s)</>
+          ) : (
+            <>&nbsp;</>
+          )}
+        </NBytes>
         <PrettyInput
           label="BIN"
           value={formatValue(value, 2, 8)}
